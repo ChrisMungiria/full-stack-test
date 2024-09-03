066dd3fa088e37330cc88c2e6b5525b91cb77bfd
ChrisMungiria <christopher.mungiria@strathmore.edu>
2024-09-04 01:59:59 +0300

    feat: connected to MongoDB

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,22 @@ const express = require("express");
 require("dotenv").config();
 const dotenv = require("dotenv");
 
+const mongoose = require("mongoose");
+
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGO_URI;
 
-app.listen(PORT, () => {
-  console.log(`App is listening on PORT: ${PORT}`);
-});
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB: ", error);
+  });
