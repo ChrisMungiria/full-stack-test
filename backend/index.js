5b14b07621907f1dbe39e807b877410d7107401d
ChrisMungiria <christopher.mungiria@strathmore.edu>
2024-09-04 02:16:47 +0300

    feat: create a user api

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,23 @@ require("dotenv").config();
 const dotenv = require("dotenv");
 
 const mongoose = require("mongoose");
+const User = require("./models/user.model");
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
+
+// Create a user
+app.post("/api/createUser", async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 mongoose
   .connect(MONGO_URI)
